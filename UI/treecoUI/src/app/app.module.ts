754c547e0847a968965a18be011539974a914082
PriyankaSsolanki <49741811+PriyankaSsolanki@users.noreply.github.com>
2020-01-15 11:01:07 +0530

    navigation and components

--- UI/treecoUI/src/app/app.module.ts
+++ UI/treecoUI/src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from'@angular/common/http';
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import {appRoutes} from './routes';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent,
+      HomeComponent,
+      AboutComponent,
+      CareerComponent,
+      ContactComponent,
+      BlogComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [],
    bootstrap: [
---
+++ UI/treecoUI/src/app/routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { CareerComponent } from './career/career.component';
+
+export const appRoutes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'career', component: CareerComponent},
+    {path: '**', redirectTo : 'home', pathMatch: 'full'}
+]
