c80b92cef5bccfb0558fbcebe6bd8f689714ee93
PriyankaSsolanki <49741811+PriyankaSsolanki@users.noreply.github.com>
2020-01-15 09:22:08 +0530

    InitialCommit

---
+++ treeCoApp.API/Controllers/AboutUsController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace treeCoApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutUsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult  Get()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new {employeeid = 01989, Name = "John Patrick"},
+                new {employeeid = 01987, Name= "Michael"},
+                new {employeeid = 01988, Name= "Akhil Mittal"}
+            });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
